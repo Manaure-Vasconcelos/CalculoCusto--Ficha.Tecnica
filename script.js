0d1f6ae550ce80a7c87d666641b30e2f1ab3bb87
Manaure Vasconcelos <manaurevasc97@gmail.com>
2023-07-01 15:35:21 -0300

    att script
    foi adc um selection

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let total = 0;
 function addFood() {
   // Retrieve form input values
   const ingredients = document.getElementById("ingredients").value;
+  const typeFood = document.getElementById("typeFood").value;
   const marketWeight = Number(document.getElementById("marketWeight").value);
   const marketPrice = Number(document.getElementById("marketPrice").value);
   const grossWeight = Number(document.getElementById("grossWeight").value);
@@ -21,13 +22,14 @@ function addFood() {
     cell1.innerHTML = ingredients;
 
     const cell2 = newRow.insertCell(1);
-    cell2.innerHTML = marketWeight;
+    cell2.innerHTML = typeFood === "1" ? `${marketWeight} g` : `${marketWeight} L`;
+    
 
     const cell3 = newRow.insertCell(2);
-    cell3.innerHTML = marketPrice;
+    cell3.innerHTML = `R$ ${marketPrice.toFixed(2)}`;
 
     const cell4 = newRow.insertCell(3);
-    cell4.innerHTML = grossWeight;
+    cell4.innerHTML = typeFood === "1" ? `${grossWeight} g` : `${grossWeight} L`;
 
     const cell5 = newRow.insertCell(4);
     let costUni = costReal(marketPrice, marketWeight, grossWeight);
@@ -38,4 +40,17 @@ function addFood() {
     result.innerHTML = `R$ ${total.toFixed(2)}`;
   }
   // Clear value inputs and focus
+}
+
+function changeTable() {
+  const typeFood = document.getElementById("typeFood").value;
+  const embalagem = document.getElementById("embalagem");
+  const uso = document.getElementById("usoReceita");
+  if (typeFood === "1") {
+    embalagem.innerHTML = "Tamanho da embalagem (Gramas)";
+    uso.innerHTML = "Uso na receita (Gramas)";
+  } else if (typeFood === "2") {
+    embalagem.innerHTML = "Tamanho da embalagem (Litros)";
+    uso.innerHTML = "Uso na receita (Litros)";
+  }
 }
