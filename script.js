1089e9698914323b064660910e46e5d7af14d914
Manaure Vasconcelos <manaurevasc97@gmail.com>
2023-07-01 17:52:03 -0300

    att script e html
    inserido um select e interação com o script

--- script.js
+++ script.js
@@ -6,14 +6,16 @@ function addFood() {
   const marketWeight = Number(document.getElementById("marketWeight").value);
   const marketPrice = Number(document.getElementById("marketPrice").value);
   const grossWeight = Number(document.getElementById("grossWeight").value);
-  const lineCostTot = document.getElementById("custoTotal");
-  const result = document.getElementById("resultado");
+  const trCustoTotal = document.getElementById("trCustoTotal");
+  //const trCustoPorcao = document.getElementById("trCustoPorcao");
+  const result = document.getElementById("custoTotal");
+
 
   // Create a new table row
   const table = document.getElementById("foodTable");
   const newRow = table.insertRow(1);
 
-  if ((ingredients, marketWeight, marketPrice, grossWeight === "")) {
+  if ((ingredients, marketWeight, marketPrice, grossWeight === "" || typeFood === "0")) {
     alert("Preencha os dados corretamente.");
   } else {
     let costReal = (preço, embalagem, uso) => (preço / embalagem) * uso;
@@ -32,11 +34,12 @@ function addFood() {
     cell4.innerHTML = typeFood === "1" ? `${grossWeight} g` : `${grossWeight} L`;
 
     const cell5 = newRow.insertCell(4);
-    let costUni = costReal(marketPrice, marketWeight, grossWeight);
+    const costUni = costReal(marketPrice, marketWeight, grossWeight);
     total += costUni;
     cell5.innerHTML = `R$ ${costUni.toFixed(2)}`;
 
-    lineCostTot.style.opacity = 1;
+    trCustoTotal.style.opacity = 1;
+    //trCustoPorcao.style.opacity = 1;
     result.innerHTML = `R$ ${total.toFixed(2)}`;
   }
   // Clear value inputs and focus
@@ -54,3 +57,10 @@ function changeTable() {
     uso.innerHTML = "Uso na receita (Litros)";
   }
 }
+
+/*function CostPor(){
+  const porcao = Number(document.getElementById("porcao").value);
+  const resultPor = document.getElementById("custoPorcao");
+
+  resultPor.innerHTML = total / porcao;
+}*/
