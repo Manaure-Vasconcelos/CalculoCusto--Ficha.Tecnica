f0959846345aca7041865b5772fe5231b6f23ec6
Manaure Vasconcelos <manaurevasc97@gmail.com>
2023-08-01 00:09:14 -0300

    Ajustes js. Ainda falta

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-let total = 0;
-
-document.addEventListener('keydown', function (event) {
-  if (event.key === 'Enter') {
-    addFood();
-  }
-})
-
-function addFood() {
-  // Retrieve form input values
+const global = (function () {
   const ingredients = document.getElementById("ingredients").value;
   const marketWeight = Number(document.getElementById("marketWeight").value);
   const marketPrice = Number(document.getElementById("marketPrice").value);
@@ -15,75 +6,83 @@ function addFood() {
   const trCustoTotal = document.getElementById("trCustoTotal");
   const Lucro = document.getElementById("Lucro");
   const result = document.getElementById("custoTotal");
+  let total = 0;
 
-  // Create a new table row
-  const table = document.getElementById("foodForm");
-  const newRow = table.insertRow(2);
+  global.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+      addFood();
+    }
+  })
 
-  if (
-    !!ingredients,
-    !!marketWeight,
-    !!marketPrice,
-    !!grossWeight === false
-  ) {
-    alert("Preencha os dados corretamente.");
-  } else {
-    // Add cells to the new row
-    const cell1 = newRow.insertCell(0);
-    cell1.innerHTML = ingredients;
-    
-    const cell2 = newRow.insertCell(1);
-    cell2.innerHTML = marketWeight;
+  function addFood() {
+    // Create a new table row
+    const table = document.getElementById("foodForm");
+    const newRow = table.insertRow(2);
 
-    const cell3 = newRow.insertCell(2);
-    cell3.innerHTML = `R$ ${marketPrice.toFixed(2)}`;
+    if (
+      !!ingredients,
+      !!marketWeight,
+      !!marketPrice,
+      !!grossWeight === false
+    ) {
+      alert("Preencha os dados corretamente.");
+    } else {
+      // Add cells to the new row
+      const cell1 = newRow.insertCell(0);
+      cell1.innerHTML = ingredients;
 
-    const cell4 = newRow.insertCell(3);
-    cell4.innerHTML = grossWeight;
+      const cell2 = newRow.insertCell(1);
+      cell2.innerHTML = marketWeight;
 
-    const cell5 = newRow.insertCell(4);
-    const costUni = costReal(marketPrice, marketWeight, grossWeight);
-    total += costUni;
-    cell5.innerHTML = `R$ ${costUni.toFixed(2)}`;
+      const cell3 = newRow.insertCell(2);
+      cell3.innerHTML = `R$ ${marketPrice.toFixed(2)}`;
 
+      const cell4 = newRow.insertCell(3);
+      cell4.innerHTML = grossWeight;
+
+      const cell5 = newRow.insertCell(4);
+      const costUni = costReal(marketPrice, marketWeight, grossWeight);
+      total += costUni;
+      cell5.innerHTML = `R$ ${costUni.toFixed(2)}`;
+
+      const cell6 = newRow.insertCell(5);
+      const btnEdit = createElement()
+      cell6.appendChild(btnEdit);
+
+      trCustoTotal.style.opacity = 1;
+      result.innerHTML = `R$ ${total.toFixed(2)}`;
+      Lucro.innerHTML = `R$ ${total.toFixed(2)}`;
+    }
+    clearInputs();
+  }
+
+  let costReal = (preço, embalagem, uso) => (preço / embalagem) * uso;
+
+  const createElement = () => {
     const btnEdit = document.createElement("button");
     btnEdit.setAttribute("id", "btnEdit");
     btnEdit.setAttribute("onclick", "editElement()");
     btnEdit.innerHTML = `<span class="material-icons">
-    edit
-    </span>`;
-    const cell6 = newRow.insertCell(5);
-    cell6.appendChild(btnEdit);
-    
-    trCustoTotal.style.opacity = 1;
-    result.innerHTML = `R$ ${total.toFixed(2)}`;
-    Lucro.innerHTML = `R$ ${total.toFixed(2)}`;
+        edit
+      </span>`;
+    return btnEdit
+  };
+
+  function clearInputs() {
+    ingredients.value = "";
+    marketWeight = "";
+    marketPrice = "";
+    grossWeight = "";
+    ingredients.focus();
   }
-  clearInputs();
-}
 
-let costReal = (preço, embalagem, uso) => (preço / embalagem) * uso;
+  function showArticle(src, event) {
+    event.preventDefault();
 
-function clearInputs() {
-  const ingredients = document.getElementById("ingredients")
-  const marketWeight = document.getElementById("marketWeight")
-  const marketPrice = document.getElementById("marketPrice")
-  const grossWeight = document.getElementById("grossWeight")
-
-  ingredients.value = "";
-  marketWeight.value = "";
-  marketPrice.value = "";
-  grossWeight.value = "";
-  ingredients.focus();
-}
-
-function showArticle(src, event) {
-  event.preventDefault();
-
-  const iframe = document.getElementById("iframe");
-  iframe.src = `conteudoIframe.html#${src}`;
-}
-
+    const iframe = document.getElementById("iframe");
+    iframe.src = `conteudoIframe.html#${src}`;
+  }
+})();
 /* function editElement() {
   const table = document.getElementById("foodTable")
   const btnEdit = document.getElementById("btnEdit");
