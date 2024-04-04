3112da947bc05776aa950f1f74f891a065bdad9f
Manaure Vasconcelos <manaurevasc97@gmail.com>
2024-04-04 08:44:01 -0300

    chore: install webpack manage

---
+++ frontend/assets/js/bundle.js
@@ -0,0 +1,13 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+
+console.log('testing');
+const person = { name: 'John', surname: 'vasconcelos' };
+
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
