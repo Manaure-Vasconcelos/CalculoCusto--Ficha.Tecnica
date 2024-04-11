44d41f99b6f188310447608ecee1e15a33d55cc4
Manaure Vasconcelos <manaurevasc97@gmail.com>
2024-04-11 17:06:40 -0300

    feat: install new dependecy, jest

---
+++ src/index.spec.ts
@@ -0,0 +1,6 @@
+describe('Primeiro test', () => {
+  it('TestOne', () => {
+    const num = 1;
+    expect(num).toBe(1);
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,8 @@
 // criar controllers para gerenciar as requisições.
-/* import { addCustoUni, addCustosFixos, addValorFinal } from './business';
-import { selectElement, formatNumber, temporaryObj } from './utils'; */
 import { TableOfIngredients } from './services/table.ingredients.service';
 import { Ingredient } from './services/ingredient.service';
 import { TableCostUnit } from './services/table.costUnit.service';
 
-// Ja inicia o programa com a instrancias criadas e passadas como props.
-// Assim fica uma dependencia muito forte.
-// Fazer abstração
 const comida1 = new Ingredient('comida1', 6, 10, 3);
 const comida2 = new Ingredient('comida2', 6, 10, 3);
 const tableOfIngredients = new TableOfIngredients(comida1);
@@ -17,6 +12,5 @@ tableOfIngredients.setIngredient(comida2);
 const tableCostUnit = new TableCostUnit(tableOfIngredients);
 tableCostUnit.servings = 2;
 tableCostUnit.packaging = 1;
-tableCostUnit.setCostUnit(); // => 6
+tableCostUnit.setCostUnit();
 console.log(tableCostUnit.costUnit); // => 6
-// Chamar pela instancia o valor dos valores de dentro dos obj.
